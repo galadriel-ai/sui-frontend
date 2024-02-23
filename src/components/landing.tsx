73144ed1c30a56fc610c0e9a1feee2ce749a94ce
kristjanpeterson <kristjan@nftport.xyz>
2024-02-23 12:25:13 +0200

    Add note about zk

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -177,7 +177,10 @@ export function Landing() {
             </>
           }
           <div
-            className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            className="fixed bottom-0 left-0 flex flex-col h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <div className="mx-auto pb-2">
+              Only regular wallets supported! (no zk support)
+            </div>
             <ConnectButton
               connectText={"Connect Wallet"}
             />
